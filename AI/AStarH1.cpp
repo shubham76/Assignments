f227c924f7066bd2b5def962f2a642be2cffc197
Shubham R Patil <spatil557@gmail.com>
2017-09-09 12:28:03 +0530

    AI Assignements
    8 puzzle solution in CPP

---
+++ AI/AStarH1.cpp
@@ -0,0 +1,202 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define N 3
+
+struct node{
+	int tile [3][3];
+	int fValue;
+	int gValue;
+	int hValue;
+};
+
+int misplacedTiles(int tile[N][N]){
+	int actVal=1, misTiles=0;
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(tile[i][j]!=actVal && !(i==2 && j==2)){
+				misTiles++;
+			}
+			actVal++;
+		}
+	}
+
+	return misTiles;
+}
+
+struct compare{
+    bool operator()(const node* a, const node* b){
+		if(a->fValue > b->fValue){
+			return 1;
+		}
+		else{
+		    return 0;
+	    }
+    }
+};
+
+bool isSolvable(int tile[N][N]){
+	int arr[9];
+	int k=0;
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			arr[k++]=tile[i][j];
+		}
+	}
+
+	int invCount=0;
+
+	for(int i=0;i<8;i++){
+		for(int j=i+1;j<9;j++){
+			if(arr[i]!=0 && arr[j]!=0 && arr[i]>arr[j]){
+				invCount++;
+			}
+		}
+	}
+
+	if(invCount%2==1){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+
+bool areAllUnique(int tile[N][N]){
+	int hist[9]={0};
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			hist[tile[i][j]]++;
+		}
+	}
+
+	for(int i=0; i<9; i++){
+		if(hist[i]==0 || hist[i]>1){
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool isFinalState(int tile[N][N]){
+	int value=1;
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(tile[i][j]==value){
+				value++;
+				if(value==9)
+					return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+node* makeNode(node* curr_head, int currX, int currY, int newX, int newY)
+{
+	int i,j,temp;
+
+	node* new_node=(node*)malloc(sizeof(node));
+	
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			new_node->tile[i][j]=curr_head->tile[i][j];
+		}
+	}
+	
+	temp=new_node->tile[currX][currY];
+	new_node->tile[currX][currY]=new_node->tile[newX][newY];
+	new_node->tile[newX][newY]=temp;
+	
+	new_node->gValue=curr_head->gValue+1;
+	new_node->hValue=misplacedTiles(new_node->tile);
+	new_node->fValue=new_node->gValue+new_node->hValue;
+	
+	return new_node;
+}
+
+int main(int argc, char const *argv[])
+{
+	node* head=(node*)malloc(sizeof(node));
+
+	cout<<"Enter intial state: Put 0 for blank state.\n";
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			int temp;
+			cin>>temp;
+			head->tile[i][j]=temp;
+		}
+	}
+
+	if(!areAllUnique(head->tile)){
+		cout<<"All the values in tile must be unique.\n";
+		exit(0);
+	}
+	if(!isSolvable(head->tile)){
+		cout<<"The given  instance of puzzle is not solvable.\n";
+		exit(0);
+	}
+
+	head->gValue=0;
+	head->hValue=misplacedTiles(head->tile);
+	head->fValue=head->gValue+head->hValue;
+
+	printf("hValue = %d\n",head->hValue);
+
+	priority_queue<node *, vector<node *>, compare> tree;
+
+	tree.push(head);
+
+	int steps=0, currX, currY;
+	while(!tree.empty()){
+		node* curr_head=tree.top();
+		tree.pop();
+
+		if(isFinalState(curr_head->tile)){
+			cout<<"The solution is found!.\n";
+			cout<<"The total number of steps required are "<<steps<<"."<<endl;
+			exit(0);
+		}
+		else{
+			for(int i=0; i<N; i++){
+				for(int j=0; j<N; j++){
+					if(curr_head->tile[i][j]==0){
+						currX=i, currY=j;
+						break;
+					}
+				}
+			}
+
+			if(currX-1>=0){
+				steps++;
+				node* new_node=makeNode(curr_head,currX,currY,currX-1,currY);
+				tree.push(new_node);
+			}
+			if(currY-1>=0){
+				steps++;
+				node* new_node=makeNode(curr_head,currX,currY,currX,currY-1);
+				tree.push(new_node);
+			}
+			if(currX+1<=2){
+				steps++;
+				node* new_node=makeNode(curr_head,currX,currY,currX+1,currY);
+				tree.push(new_node);
+			}
+			if(currY+1<=2){
+				steps++;
+				node* new_node=makeNode(curr_head,currX,currY,currX,currY+1);
+				tree.push(new_node);
+			}
+		}
+	}
+	return 0;
+}
---
+++ AI/IDS.cpp
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+
+#define N 3
+
+using namespace std;
+
+struct node{
+	int tile [N][N];
+	int level;
+};
+
+bool isSolvable(int tile[N][N]){
+	int arr[9];
+	int k=0;
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			arr[k++]=tile[i][j];
+		}
+	}
+
+	int invCount=0;
+
+	for(int i=0;i<8;i++){
+		for(int j=i+1;j<9;j++){
+			if(arr[i]!=0 && arr[j]!=0 && arr[i]>arr[j]){
+				invCount++;
+			}
+		}
+	}
+
+	if(invCount%2==1){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+
+bool areAllUnique(int tile[N][N]){
+	int hist[9]={0};
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			hist[tile[i][j]]++;
+		}
+	}
+
+	for(int i=0; i<9; i++){
+		if(hist[i]==0 || hist[i]>1){
+			return false;
+		}
+	}
+
+	return true;
+}
+
+node* makeNode(node* curr_head, int currX, int currY, int newX, int newY){
+	int i,j,temp;
+
+	node* new_node=(node*)malloc(sizeof(node));
+	
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			new_node->tile[i][j]=curr_head->tile[i][j];
+		}
+	}
+	
+	temp=new_node->tile[currX][currY];
+	new_node->tile[currX][currY]=new_node->tile[newX][newY];
+	new_node->tile[newX][newY]=temp;
+	
+	new_node->level=curr_head->level+1;
+
+	return new_node;
+}
+
+bool isFinalState(int tile[N][N]){
+	int value=1;
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(tile[i][j]==value){
+				value++;
+				if(value==9)
+					return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+int main(){
+	node* head=(node*)malloc(sizeof(node));
+
+	cout<<"Enter intial state: Put 0 for blank state.\n";
+
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			int temp;
+			cin>>temp;
+			head->tile[i][j]=temp;
+		}
+	}
+
+	if(!areAllUnique(head->tile)){
+		cout<<"All the values in tile must be unique.\n";
+		exit(0);
+	}
+	if(!isSolvable(head->tile)){
+		cout<<"The given  instance of puzzle is not solvable.\n";
+		exit(0);
+	}
+
+	head->level=0;
+
+	int steps=0, currX,currY;
+	for(int IDSLimits=0;;IDSLimits++){
+		stack<node *> tree;
+
+		tree.push(head);
+
+		while(!tree.empty()){
+			node* curr_head=tree.top();
+			tree.pop();
+
+			if(isFinalState(curr_head->tile)){
+				cout<<"The solution is found!.\n";
+				cout<<"The total number of steps required are "<<steps<<"."<<endl;
+				exit(0);
+			}
+			else{
+				if(curr_head->level < IDSLimits){
+					for(int i=0; i<N; i++){
+						for(int j=0; j<N; j++){
+							if(curr_head->tile[i][j]==0){
+								currX=i, currY=j;
+								break;
+							}
+						}
+					}
+					if(currX-1>=0){
+						steps++;
+						node* new_node=makeNode(curr_head,currX,currY,currX-1,currY);
+						tree.push(new_node);
+					}
+					if(currY-1>=0){
+						steps++;
+						node* new_node=makeNode(curr_head,currX,currY,currX,currY-1);
+						tree.push(new_node);
+					}
+					if(currX+1<=2){
+						steps++;
+						node* new_node=makeNode(curr_head,currX,currY,currX+1,currY);
+						tree.push(new_node);
+					}
+					if(currY+1<=2){
+						steps++;
+						node* new_node=makeNode(curr_head,currX,currY,currX,currY+1);
+						tree.push(new_node);
+					}
+				}
+			}
+		}
+	}
+}
